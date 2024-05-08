7510c194c5cb159c96f41fc7cfab9f7d769e7bcc
Nicolás González <nicolook.pro@gmail.com>
2024-05-08 09:55:25 -0300

    improve Menu with data from Headless

--- src/js/globals.js
+++ src/js/globals.js
@@ -1,7 +1,7 @@
 import checkDevice from '../js/checkDevice'
 import smoothScroll from "../js/smoothscroll"
 import scrollMarkers from "../js/scrollMarkers"
-import menuScrollStatus from '../js/menuScrollStatus'
+import smoothAnchors from '../js/smoothAnchors'
 import smartMenu from "../js/smartMenu"
 // import menuMobile from '../js/menuMobile'
 
@@ -63,11 +63,11 @@ document.addEventListener('scroll', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-	menuScrollStatus();
+	smoothAnchors();
 });
 
 document.addEventListener('astro:after-swap', function () {
-	menuScrollStatus();
+	smoothAnchors();
 });
 
 
